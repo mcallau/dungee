9da3ef93a4af6773adf5210a67e26b617bebb20b
mcallau <113464181+mcallau@users.noreply.github.com>
2023-04-24 16:48:37 +0200

    .

--- api/damelosusers.js
+++ api/damelosusers.js
@@ -1,5 +1,5 @@
 export default function handler(request,reponse){
-fetch("https://dungee-23819-default-rtdb.europe-west1.firebasedatabase.app")
+fetch('https://dungee-23819-default-rtdb.europe-west1.firebasedatabase.app')
 .then (eljsonperocomstring=>response.send(eljsonperocomstring))
    
 }
