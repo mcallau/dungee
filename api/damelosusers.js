dca7f515fab74e096943307802207bb30591fce8
mcallau <113464181+mcallau@users.noreply.github.com>
2023-04-25 15:31:01 +0200

    .

--- api/damelosusers.js
+++ api/damelosusers.js
@@ -1,5 +1,5 @@
 export default function handler(request, response){
-    fetch('https://dungee-23819-default-rtdb.europe-west1.firebasedatabase.app/pins.json')
+    fetch('https://dungee-23819-default-rtdb.europe-west1.firebasedatabase.app/pins/PIN${pin}.json')
     .then (datos => datos.json())
     .then (datosjson => response.json(datosjson))
    
